b57f65eb839abbf677caaa6d41429d292cb5e69b
vaibhavrathore-dev <vaibhavrathore911@gmail.com>
2026-08-22 11:41:47 +0530

    Chaged the none to base.metadata in alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from app.database import Base
 
 from app.core.config import settings
 
@@ -14,7 +15,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 
-target_metadata = None
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
