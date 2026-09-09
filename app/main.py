019bf38e10b89e39eb224876e753bd8a8a916c6a
vaibhavrathore-dev <vaibhavrathore911@gmail.com>
2026-09-09 21:25:39 +0530

    Added cors to increase security

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 
 from app.routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="BUZZ API",
     version="1.0.0"
+)
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
